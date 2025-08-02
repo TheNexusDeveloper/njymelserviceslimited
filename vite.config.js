25a928728920e14ccb8c5beb39b598210f5319c4
Kosy <okekekosy12@gmail.com>
2025-08-03 00:56:29 +0100

    Base url fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/njymelserviceslimited/', // Set the base path for your project
+  base: '/', // Set the base path for your project
 })
